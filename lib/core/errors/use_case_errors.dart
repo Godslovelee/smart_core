c67cd68be24320703a3b18ccf0d8998dadf3f19a
Godslove Lee <godslovelee@yahoo.com>
2023-02-15 02:47:58 +0100

    add use-case error

---
+++ lib/core/errors/use_case_errors.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: camel_case_types
+
+enum authProblems {
+  userNotFound,
+  passwordNotValid,
+  networkError,
+  errorSmsCode,
+  smsCodeExpired,
+  userAlreadyExist,
+  invalidPhoneNumber,
+  undefined,
+}
+enum authCaseProblems {
+  signInEmail,
+  signInPhone,
+  googleSignIn,
+  appleSinIn,
+  createUserEmail,
+  createUserEmailSms,
+  createPhone,
+  resetPassword,
+  resendPhoneNumberSmsCode,
+  checkIfUserExist,
+  editNotificationAsRead,
+
+}
